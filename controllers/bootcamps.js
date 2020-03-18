b7d921ab80dd94d502b4d6eb17c7b1ee2b2e0293
Shoaib <shoaib_masood@rocketmail.com>
2020-03-19 04:51:57 +0500

    created Bootcamp model and added POST and GET request to bootcamp controller methods

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,24 +1,48 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc        GET show all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        GET single bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show  single bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Create new bootcamp
 // @route       POST /api/v1/bootcamps
 // @access      Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp ' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Update bootcamp
