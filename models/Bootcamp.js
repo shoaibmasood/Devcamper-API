8a7088d476f196ca1d01293a0875fa5ff9f78cb8
Shoaib <shoaib_masood@rocketmail.com>
2020-04-24 08:45:34 +0500

    added Mongoose middleware/hooks (Document middleware) and updated slug field in bootcamp model

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -6,52 +7,52 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, 'Please add a name'],
     unique: true,
     trim: true,
-    maxlength: [50, 'Name cannot be more than 50 Characters']
+    maxlength: [50, 'Name cannot be more than 50 Characters'],
   },
   slug: String,
   description: {
     type: String,
     required: [true, 'Please add description'],
-    maxlength: [500, 'Description cannot be more than 500 Characters']
+    maxlength: [500, 'Description cannot be more than 500 Characters'],
   },
   website: {
     type: String,
     match: [
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      'Please use a valid URL with HTTP or HTTPS'
-    ]
+      'Please use a valid URL with HTTP or HTTPS',
+    ],
   },
   phone: {
     type: String,
-    maxlength: [20, 'Phone number can not be longer than 20 characters']
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
   },
   email: {
     type: String,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email'
-    ]
+      'Please add a valid email',
+    ],
   },
   address: {
     type: String,
-    required: [true, 'Please add an address']
+    required: [true, 'Please add an address'],
   },
   location: {
     // GeoJSON Point
     type: {
       type: String,
-      enum: ['Point']
+      enum: ['Point'],
     },
     coordinates: {
       type: [Number],
-      index: '2dsphere'
+      index: '2dsphere',
     },
     formattedAddress: String,
     street: String,
     city: String,
     state: String,
     zipcode: String,
-    country: String
+    country: String,
   },
 
   careers: {
@@ -64,39 +65,48 @@ const BootcampSchema = new mongoose.Schema({
       'UI/UX',
       'Data Science',
       'Business',
-      'Other'
-    ]
+      'Other',
+    ],
   },
   averageRating: {
     type: Number,
     min: [1, 'Rating must be at least 1'],
-    max: [10, 'Rating must can not be more than 10']
+    max: [10, 'Rating must can not be more than 10'],
   },
   averageCost: Number,
   photo: {
     type: String,
-    default: 'no-photo.jpg'
+    default: 'no-photo.jpg',
   },
   housing: {
     type: Boolean,
-    default: false
+    default: false,
   },
   jobAssistance: {
     type: Boolean,
-    default: false
+    default: false,
   },
   jobGuarantee: {
     type: Boolean,
-    default: false
+    default: false,
   },
   acceptGi: {
     type: Boolean,
-    default: false
+    default: false,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+});
+
+// Create bootcamp slug from  the name
+
+//pre is used for before the document  is saved
+
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
